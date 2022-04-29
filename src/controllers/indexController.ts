99af05f82ce84e41175577e1acb546e397d439e4
NaanAvelinoCardoso <naanavelino2018@gmail.com>
2022-04-29 16:00:31 -0300

    Testando Dados 2.2

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -3,10 +3,10 @@ import { Request, Response } from 'express';
 import User from '../models/User';
 
 export const home = async (req: Request, res: Response) => {
-    let usuarios = await User.find({});
+    let users = await User.find({});
 
 
     res.render('pages/home', {
-        usuarios
+        users
     });
 };
