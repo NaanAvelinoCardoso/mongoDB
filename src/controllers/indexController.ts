a875caada73ee78638d45d96a6f8c89b162e583a
NaanAvelinoCardoso <naanavelino2018@gmail.com>
2022-05-02 16:29:40 -0300

    Consultando Dados

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -3,8 +3,9 @@ import { Request, Response } from 'express';
 import User from '../models/User';
 
 export const home = async (req: Request, res: Response) => {
-    let users = await User.find({});
-
+    let users = await User.find({
+        age: { $gte: 18 }
+    }).sort({ age: 1 });
 
     res.render('pages/home', {
         users
