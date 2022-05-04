c14ab99760e8000bd9a284fd40f84b1a5b65fadb
NaanAvelinoCardoso <naanavelino2018@gmail.com>
2022-05-04 17:08:23 -0300

    Inserindo Usuarios no BD

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -3,9 +3,8 @@ import { Request, Response } from 'express';
 import User from '../models/User';
 
 export const home = async (req: Request, res: Response) => {
-    let users = await User.find({
-        age: { $gte: 18 }
-    }).skip(0).limit(2);
+
+    let users = await User.find({});
 
     res.render('pages/home', {
         users
